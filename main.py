2f313a71ab4e15f65803a70f95119b4b9691fc8c
Zoran Bukorac <zoran00.zb@gmail.com>
2021-01-04 10:40:51 +0100

    Added the results of the third run and fourth run

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import ann_criterion as crit
 import pso
+import time
 
 if __name__ == '__main__':
     opt = pso.MyOptions()
-    opt.npart = 100
-    opt.niter = 1000
     for i in range(1):
         print("Test number " + str(i + 1))
+        t = time.time()
         pso.PSO(crit.optimality_criterion, 60, opt)
+        print(time.time() - t)
         print("\n\n")
