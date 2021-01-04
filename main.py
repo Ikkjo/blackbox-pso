d913880d4969685bc2973211a059bb5076bea112
ikkjo <ilija.kalinic@gmail.com>
2021-01-04 10:20:51 +0100

    Updates main.py
    Adds optimization_third_run.txt

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import pso
 
 if __name__ == '__main__':
     opt = pso.MyOptions()
-    opt.npart = 100
-    opt.niter = 1000
-    for i in range(1):
-        print("Test number " + str(i + 1))
-        pso.PSO(crit.optimality_criterion, 60, opt)
-        print("\n\n")
+    opt.npart = 150
+    opt.niter = 100
+    pso.PSO(crit.optimality_criterion, 60, opt)
+
